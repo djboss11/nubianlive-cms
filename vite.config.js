23a313293c9b53d16a186800b8ed62c403e2c426
djboss11 <leverettmedia@gmail.com>
2026-03-07 21:44:18 -0500

    Fix vite config

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { copyFileSync } from 'fs'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    {
-      name: 'copy-redirects',
-      closeBundle() {
-        copyFileSync('public/_redirects', 'dist/_redirects')
-      }
-    }
-  ],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: undefined
-      }
-    }
-  }
+  plugins: [react()],
 })
